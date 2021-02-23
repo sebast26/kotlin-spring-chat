b776ce93f2d4f9251148db599d60884e05ecd5f6
Sebastian Gorecki <sebastian.gorecki@olx.pl>
2021-02-23 18:57:22 +0100

    Adding WebFlux and R2DBC

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,9 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	runtimeOnly("com.h2database:h2")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("io.r2dbc:r2dbc-h2")
 
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
--- src/test/kotlin/com/example/kotlin/chat/ChatKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/kotlin/chat/ChatKotlinApplicationTests.kt
@@ -5,6 +5,8 @@ import com.example.kotlin.chat.repository.Message
 import com.example.kotlin.chat.repository.MessageRepository
 import com.example.kotlin.chat.service.MessageVM
 import com.example.kotlin.chat.service.UserVM
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -26,7 +28,7 @@ import java.time.temporal.ChronoUnit.MILLIS
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = [
-        "spring.datasource.url=jdbc:h2:mem:testdb"
+        "spring.r2dbc.url=r2dbc:h2:mem:///testdb;USER=sa;PASSWORD=password"
     ]
 )
 class ChatKotlinApplicationTests {
@@ -43,39 +45,43 @@ class ChatKotlinApplicationTests {
 
     @BeforeEach
     fun setUp() {
-        val secondBeforeNow = now.minusSeconds(1)
-        val twoSecondBeforeNow = now.minusSeconds(2)
-        val savedMessages = messageRepository.saveAll(
-            listOf(
-                Message(
-                    "*testMessage*",
-                    ContentType.PLAIN,
-                    twoSecondBeforeNow,
-                    "test",
-                    "http://test.com"
-                ),
-                Message(
-                    "**testMessage2**",
-                    ContentType.MARKDOWN,
-                    secondBeforeNow,
-                    "test1",
-                    "http://test.com"
-                ),
-                Message(
-                    "`testMessage3`",
-                    ContentType.MARKDOWN,
-                    now,
-                    "test2",
-                    "http://test.com"
+        runBlocking {
+            val secondBeforeNow = now.minusSeconds(1)
+            val twoSecondBeforeNow = now.minusSeconds(2)
+            val savedMessages = messageRepository.saveAll(
+                listOf(
+                    Message(
+                        "*testMessage*",
+                        ContentType.PLAIN,
+                        twoSecondBeforeNow,
+                        "test",
+                        "http://test.com"
+                    ),
+                    Message(
+                        "**testMessage2**",
+                        ContentType.MARKDOWN,
+                        secondBeforeNow,
+                        "test1",
+                        "http://test.com"
+                    ),
+                    Message(
+                        "`testMessage3`",
+                        ContentType.MARKDOWN,
+                        now,
+                        "test2",
+                        "http://test.com"
+                    )
                 )
             )
-        )
-        lastMessageId = savedMessages.first().id ?: ""
+            lastMessageId = savedMessages.first().id ?: ""
+        }
     }
 
     @AfterEach
     fun tearDown() {
-        messageRepository.deleteAll()
+        runBlocking {
+            messageRepository.deleteAll()
+        }
     }
 
     @ParameterizedTest
@@ -117,28 +123,30 @@ class ChatKotlinApplicationTests {
 
     @Test
     fun `test that messages posted to the API is stored`() {
-        client.postForEntity<Any>(
-            URI("/api/v1/messages"),
-            MessageVM(
-                "`HelloWorld`",
-                UserVM("test", URL("http://test.com")),
-                now.plusSeconds(1)
+        runBlocking {
+            client.postForEntity<Any>(
+                URI("/api/v1/messages"),
+                MessageVM(
+                    "`HelloWorld`",
+                    UserVM("test", URL("http://test.com")),
+                    now.plusSeconds(1)
+                )
             )
-        )
 
-        messageRepository.findAll()
-            .first { it.content.contains("HelloWorld") }
-            .apply {
-                assertThat(this.prepareForTesting())
-                    .isEqualTo(
-                        Message(
-                            "`HelloWorld`",
-                            ContentType.MARKDOWN,
-                            now.plusSeconds(1).truncatedTo(MILLIS),
-                            "test",
-                            "http://test.com"
+            messageRepository.findAll()
+                .first { it.content.contains("HelloWorld") }
+                .apply {
+                    assertThat(this.prepareForTesting())
+                        .isEqualTo(
+                            Message(
+                                "`HelloWorld`",
+                                ContentType.MARKDOWN,
+                                now.plusSeconds(1).truncatedTo(MILLIS),
+                                "test",
+                                "http://test.com"
+                            )
                         )
-                    )
-            }
+                }
+        }
     }
 }
